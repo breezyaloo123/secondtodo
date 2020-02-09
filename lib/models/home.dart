df950311aeda53e67757b432cc0235542e860cbf
breezyaloo123 <alassanesall771@gmail.com>
2020-02-09 02:13:21 +0100

    task

--- lib/models/home.dart
+++ lib/models/home.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeApp extends StatefulWidget {
@@ -33,6 +34,66 @@ class _HomeAppState extends State<HomeApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: SpeedDial(
+        marginRight: 18,
+        marginBottom: 20,
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: IconThemeData(size: 22.0),
+        closeManually: false,
+        curve: Curves.bounceIn,
+        overlayColor: Colors.black,
+        overlayOpacity: 0.5,
+        onOpen: ()=>print('OPENING DIAL'),
+        onClose: () =>print('Dial closed'),
+        tooltip: 'speed dial',
+        heroTag: 'speed !!!',
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 8.0,
+        shape: CircleBorder(),
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.book),
+            backgroundColor: Colors.red,
+            label: 'read',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: ()
+            {
+
+            }
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.book),
+            backgroundColor: Colors.red,
+            label: 'read',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: ()
+            {
+
+            }
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.book),
+            backgroundColor: Colors.red,
+            label: 'read',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: ()
+            {
+
+            }
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.book),
+            backgroundColor: Colors.red,
+            label: 'read',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: ()
+            {
+
+            }
+          ),
+        ],
+      ),
      appBar: AppBar(
        title: Text("HOME"),
        actions: <Widget>[
